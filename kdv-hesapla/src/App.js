1a698e93292b1ee022215c0186f1c8278e7baa42
Kadir Aydın <kadiraydn01@gmail.com>
2025-01-14 02:02:25 +0300

    build

--- kdv-hesapla/src/App.js
+++ kdv-hesapla/src/App.js
@@ -1,119 +1,99 @@
-// App.js
 import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
   const [amount, setAmount] = useState("");
-  const [months, setMonths] = useState(1);
-  const [installmentData, setInstallmentData] = useState(null);
+  const [installments, setInstallments] = useState([]);
 
-  const handleCalculate = () => {
-    if (amount && months) {
-      const rate = installmentRates[months];
-      const totalAmount = (amount * rate).toFixed(2);
-      const monthlyPayment = (totalAmount / months).toFixed(2);
-      setInstallmentData({ totalAmount, monthlyPayment });
-    }
+  const handleCalculateInstallments = () => {
+    if (!amount) return;
+
+    const calculatedInstallments = Object.keys(installmentRates).map(
+      (month) => {
+        const rate = installmentRates[month];
+        const totalAmount = (amount * rate).toFixed(2);
+        const monthlyPayment = (totalAmount / month).toFixed(2);
+
+        return {
+          month,
+          totalAmount,
+          monthlyPayment,
+        };
+      }
+    );
+
+    setInstallments(calculatedInstallments);
   };
 
   return (
-    <div className="app bg-gray-100 min-h-screen flex flex-col items-center py-8">
-      <header className="header text-center mb-6">
-        <h1 className="text-3xl font-bold text-indigo-600">
-          Kredi Kartı Taksit Hesaplama
-        </h1>
-      </header>
-      <main className="main bg-white p-6 rounded-lg shadow-lg w-11/12 max-w-md">
-        <InstallmentCalculator
-          amount={amount}
-          setAmount={setAmount}
-          months={months}
-          setMonths={setMonths}
-          onCalculate={handleCalculate}
-        />
-        {installmentData && (
-          <InstallmentDetails
-            totalAmount={installmentData.totalAmount}
-            monthlyPayment={installmentData.monthlyPayment}
-            months={months}
-          />
+    <div className="relative flex flex-col items-center min-h-screen py-8 bg-gray-100 ">
+      <div className="absolute inset-0 bg-no-repeat bg-cover opacity-10"></div>
+      <header className="mb-6 text-center header"></header>
+      <main className="relative z-10 w-11/12 max-w-md p-6 bg-white rounded-lg shadow-lg main">
+        <div>
+          <img src="emin.jpg" alt="Emin İletişim" />
+        </div>
+        <div className="space-y-4 text-center">
+          <label className="flex flex-col">
+            <span className="my-3 font-bold text-gray-700 ">
+              Alışveriş Tutarı (TL):
+            </span>
+            <input
+              type="text"
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+              placeholder="Tutarı giriniz"
+              className="flex px-3 py-2 mt-2 text-center bg-white border border-gray-300 rounded-md shadow-sm hover:bg-slate-100 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+            />
+          </label>
+          <button
+            onClick={handleCalculateInstallments}
+            className="flex px-4 py-2 mx-auto mt-3 text-center text-white bg-indigo-600 rounded-md hover:bg-indigo-700"
+          >
+            Hesapla
+          </button>
+        </div>
+        {installments.length > 0 && (
+          <div className="mt-6 text-center details">
+            <h2 className="py-3 mb-4 text-xl font-bold text-gray-700 border border-blue-400 rounded-2xl">
+              Hesaplama Sonuçları
+            </h2>
+            <table className="w-full border border-collapse border-gray-300 table-auto">
+              <thead>
+                <tr className="bg-gray-200">
+                  <th className="px-4 py-2 border border-gray-300">
+                    Taksit Sayısı
+                  </th>
+                  <th className="px-4 py-2 border border-gray-300">
+                    Toplam Tutar
+                  </th>
+                  <th className="px-4 py-2 border border-gray-300">
+                    Aylık Taksit
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {installments.map(({ month, totalAmount, monthlyPayment }) => (
+                  <tr key={month} className="hover:bg-gray-100">
+                    <td className="px-4 py-2 border border-gray-300">
+                      {month === "1" ? "Peşin" : `${month} Ay`}
+                    </td>
+                    <td className="px-4 py-2 border border-gray-300">
+                      {totalAmount}
+                    </td>
+                    <td className="px-4 py-2 border border-gray-300">
+                      {monthlyPayment}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         )}
       </main>
     </div>
   );
 };
-
-const InstallmentCalculator = ({
-  amount,
-  setAmount,
-  months,
-  setMonths,
-  onCalculate,
-}) => (
-  <div className="calculator space-y-4">
-    <label className="block">
-      <span className="text-gray-700">Alışveriş Tutarı (TL):</span>
-      <input
-        type="number"
-        value={amount}
-        onChange={(e) => setAmount(e.target.value)}
-        placeholder="Tutarı giriniz"
-        className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-      />
-    </label>
-    <label className="block">
-      <span className="text-gray-700">Taksit Sayısı:</span>
-      <select
-        value={months}
-        onChange={(e) => setMonths(Number(e.target.value))}
-        className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-      >
-        <option value={1}>Peşin</option>
-        {Object.keys(installmentRates).map(
-          (month) =>
-            month > 1 && (
-              <option key={month} value={month}>
-                {month} Ay
-              </option>
-            )
-        )}
-      </select>
-    </label>
-    <button
-      onClick={onCalculate}
-      className="w-full bg-indigo-600 text-white py-2 px-4 rounded-lg shadow hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-    >
-      Hesapla
-    </button>
-  </div>
-);
-
-const InstallmentDetails = ({ totalAmount, monthlyPayment, months }) => (
-  <div className="details mt-6 text-center">
-    <h2 className="text-xl font-bold border border-blue-400 rounded-2xl py-3 text-gray-700 mb-4">
-      Hesaplama Sonucu
-    </h2>
-    <p className="text-gray-600 border bg-blue-100 cursor-default hover:bg-blue-300 border-blue-400 rounded-2xl py-3 mb-2">
-      Toplam Tutar:{" "}
-      <span className="font-bold text-indigo-600">{totalAmount} TL</span>
-    </p>
-    <p className="text-gray-600 border bg-blue-100 cursor-default hover:bg-blue-300 border-blue-400 rounded-2xl py-3 mb-4">
-      Aylık Taksit:{" "}
-      <span className="font-bold text-indigo-600">{monthlyPayment} TL</span>
-    </p>
-    <div className="tabs grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
-      {Array.from({ length: months }, (_, i) => (
-        <div
-          key={i}
-          className="tab bg-slate-100 hover:bg-slate-300 cursor-default p-4 rounded-lg shadow"
-        >
-          <h3 className="text-gray-700 text-center font-medium">{i + 1}. Ay</h3>
-          <p className="text-gray-600 text-center">{monthlyPayment} TL</p>
-        </div>
-      ))}
-    </div>
-  </div>
-);
 
 const installmentRates = {
   1: 1.0372,
